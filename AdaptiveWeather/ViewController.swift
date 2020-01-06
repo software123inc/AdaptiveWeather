ade0c8ede5714d51d80295808bfa7e130cb15c1c
Tim Newton <tim.newton@eduserve.us>
2020-01-06 15:32:45 -0800

    Initial Commit

---
+++ AdaptiveWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Tim Newton on 1/6/20.
+//  Copyright © 2020 EduServe, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
